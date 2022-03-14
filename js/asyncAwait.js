0c713615969a7d8efca5903645c06eaaf6bc78f3
chloeWieser <chloerosewieser@gmail.com>
2022-03-14 10:16:21 -0400

    hmk

--- js/asyncAwait.js
+++ js/asyncAwait.js
@@ -1,5 +1,17 @@
 
 
+let listGroupContainer = document.querySelector('.list-group');
+let liTagsHTMLFragment = "";
+
+let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+    keyboard: false
+})
+
+let modalTitle = document.querySelector('#exampleModalLabel')
+let modalBody = document.querySelector('.modal-body')
+
+let closeModalButtons = document.querySelector('.modal-content')
+
 const getChars = async () => {   //this arrow function is the same as writing async function getChar(){    new line}
 
     let charArr = [];
@@ -23,9 +35,53 @@ const getChars = async () => {   //this arrow function is the same as writing as
     console.log(charArr);
 
     //dom manipulation
+    charArr.forEach(charObj=>{
+        console.log({charObj})
+        liTagsHTMLFragment += `<a href="${charObj.url}" class="list-group-item list-group-item-action">${charObj.name} <b>Houses: </b> ${charObj.allegiances.length}</a>`
+    })
+
+    listGroupContainer.innerHTML = liTagsHTMLFragment;
+
+
 
     
 }
+
+listGroupContainer.addEventListener('click', async e=>{
+    e.preventDefault();
+    // console.log(e.target.href);
+    let detailedAPIEndpoint = e.target.href;
+    let apiCall = await fetch(detailedAPIEndpoint);
+    let detailedInfo = await apiCall.json();
+    modalTitle.innerHTML = detailedInfo.name; //NAME OF CHARACTER
+    modalBody.innerHTML = ""
+
+    // console.log(detailedInfo);
+
+    if(detailedInfo.allegiances.length > 0){
+        detailedInfo.allegiances.forEach(houseUrl =>{
+            displayHouseInformation(houseUrl)
+        })
+
+    }
+
+    myModal.show()
+
+})
+
+const displayHouseInformation = async houseUrl =>{
+
+    let houseAPICall = await fetch(houseUrl)
+    let houseInfo = await houseAPICall.json()
+
+    console.log(houseInfo.name);
+    //takeinformation and display in modal
+    modalBody.innerHTML += `<br>${houseInfo.name}`
+}
+
+closeModalButtons.addEventListener('click',()=>{
+    myModal.hide()
+})
 
 getChars()   //async- meaning if there is any synchronous code afterwoard, that will get executed first- so we must do our DOM manipulation within the getChars function
 
